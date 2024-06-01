72389ffbb736a69b84df8b72ccceda1e30f06ffe
Mihael Hajnić <mihael.hajnic@gmail.com>
2024-06-02 00:22:12 +0200

    BattleSys - update 1.0
    Implementacija jednostavnog combat sustava
    Obavezna dorada grafičkog sučelja
     - Popup kad se mijenja game state
     - Slidere urediti
     - Dodati više botuna - više akcija
     - action bar kompletno promjeniti
    Potrebno ubaciti enemy side combat logiku
    closes #25

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -10,24 +10,26 @@ public class BattleSystem : MonoBehaviour
 
     public GameObject playerPrefab;
     public GameObject enemyPrefab;
-
     public Transform PlayerBattleground;
     public Transform EnemyBattleground;
-
     public Text dialogueText;
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
     Unit playerUnit;
     Unit enemyUnit;
     
     public BattleState state;
-    // Start is called before the first frame update
+    // Postavimo state na start - početak combat-a
     void Start()
     {
         state = BattleState.START;
 
-        SetupBattle();
+        // koristimo coroutine da čekamo
+        StartCoroutine(SetupBattle());
     }
 
-    void SetupBattle()
+    // Instanciramo player i enemy unit-e te započnemo igračev turn
+    IEnumerator SetupBattle()
     {
         GameObject playerGO = Instantiate(playerPrefab, PlayerBattleground);
         playerUnit = playerGO.GetComponent<Unit>();
@@ -37,5 +39,80 @@ public class BattleSystem : MonoBehaviour
 
         dialogueText.text = "A wild " + enemyUnit.unitName + "\n approaches...";
 
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+
+        // čekamo dvije sekunde
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+
     }
+
+    
+    // ENEMY TURN
+    IEnumerator EnemyTurn(){
+        //TODO
+        //Implementirati kompletnu logiku borbe
+        dialogueText.text = enemyUnit.unitName + " attacks!";
+
+        yield return new WaitForSeconds(2f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.setHP(playerUnit.currentHP);
+        yield return new WaitForSeconds(2f);
+
+        if(isDead){
+            state = BattleState.LOST;
+            EndBattle();
+        } else {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }
+
+    // PLAYER TURN
+    void PlayerTurn(){
+        dialogueText.text = "Choose an action:";
+    }
+
+    public void OnAttackButton(){
+        if (state != BattleState.PLAYERTURN) return;
+
+        StartCoroutine(PlayerAttack());
+    }
+    IEnumerator PlayerAttack(){
+
+            // Napasti neprijatelja
+            bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+            enemyHUD.setHP(enemyUnit.currentHP);
+            dialogueText.text = "The attack is successful!";
+            yield return new WaitForSeconds(2f);
+
+            //Provjeriti ako je neprijatelj mrtav
+            if(isDead){
+                //Kraj bitke
+                state = BattleState.WON;
+                EndBattle();
+            } else {
+                //Neprijatelj je na redu
+                state = BattleState.ENEMYTURN;
+                StartCoroutine(EnemyTurn());
+            }
+            // Promjeniti state igre zavisno od rezultata 
+        }
+
+    // GAME STATE
+    // TODO
+    // Implementirati popup pri pobjedi/porazu
+    void EndBattle(){
+            if(state == BattleState.WON){
+                dialogueText.text = "You won the battle!";
+            } else if (state == BattleState.LOST){
+                dialogueText.text = "You lost the battle :(";
+            }
+        }
 }

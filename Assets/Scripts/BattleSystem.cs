dc6f9ea4bb035fe27070b008b2689a5525107dbf
Mihael Hajnić <mihael.hajnic@gmail.com>
2024-06-01 22:34:09 +0200

    BattleSys - mid update patch

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
@@ -13,6 +14,10 @@ public class BattleSystem : MonoBehaviour
     public Transform PlayerBattleground;
     public Transform EnemyBattleground;
 
+    public Text dialogueText;
+    Unit playerUnit;
+    Unit enemyUnit;
+    
     public BattleState state;
     // Start is called before the first frame update
     void Start()
@@ -24,8 +29,13 @@ public class BattleSystem : MonoBehaviour
 
     void SetupBattle()
     {
-        Instantiate(playerPrefab, PlayerBattleground);
-        Instantiate(enemyPrefab, EnemyBattleground);
+        GameObject playerGO = Instantiate(playerPrefab, PlayerBattleground);
+        playerUnit = playerGO.GetComponent<Unit>();
+        
+        GameObject enemyGO = Instantiate(enemyPrefab, EnemyBattleground);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = "A wild " + enemyUnit.unitName + "\n approaches...";
 
     }
 }
